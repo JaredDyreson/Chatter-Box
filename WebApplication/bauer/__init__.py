8e84191c1d000bf51153dc3e8af4585c8814d912
JaredDyreson <jared.dyreson@gmail.com>
2020-11-07 11:57:48 -0800

    the great purge has begun

--- WebApplication/bauer/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_cors import CORS, cross_origin
-
-
-app = Flask(__name__)
-CORS(app)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-
-from bauer import routes
