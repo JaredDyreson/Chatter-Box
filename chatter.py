1b5919ed349a0772b4ce75e749f525e053290624
JaredDyreson <jared.dyreson@gmail.com>
2020-11-07 11:55:55 -0800

    we have a working model

--- chatter.py
+++ chatter.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3.8
 
 from Chatter.Client import Client
-from Chatter.Server import Server
 from Chatter.Equations import Generator
+from Chatter.Game import Game
 
 C1 = Client("JARED")
 C2 = Client("JOHN")
 
-S = Server()
-S.start(C1, C2)
+G = Game()
+G.start(C1, C2)
---
+++ pingServer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3.8
+
+from websocket import create_connection
+import json
+
+ws = create_connection("ws://144.202.127.25:8080")
+print("got connection to my server")
+# ws.send(4)
+
+result =  ws.recv()
+print(result)
+ws.close()
